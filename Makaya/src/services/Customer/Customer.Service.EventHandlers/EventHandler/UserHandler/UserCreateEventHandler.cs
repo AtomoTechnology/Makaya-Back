eab2d29efd442fa2101ce18b7077fcd30d8e6e04
quchallenge2021 <quchallenge2021@gmail.com>
2021-09-24 22:19:27 -0300

    first commit

---
+++ Makaya/src/services/Customer/Customer.Service.EventHandlers/EventHandler/UserHandler/UserCreateEventHandler.cs
@@ -0,0 +1,76 @@
+﻿using Customer.Domain;
+using Customer.Service.EventHandlers.Commands.User;
+using Customers.Persistence.DataBase;
+using Makaya.Resolver.Enum;
+using Makaya.Resolver.Eum;
+using Makaya.Resolver.IExceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Customer.Service.EventHandlers.EventHandler.UserHandler
+{
+    public class UserCreateEventHandler :
+        INotificationHandler<UserCreateCommand>
+    {
+        private readonly ApplicationDbContext _context;
+        public UserCreateEventHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(UserCreateCommand comands, CancellationToken cancellationToken)
+        {
+            try
+            {
+                if (comands.AccountId == 0 || comands.AccountId == null)
+                    throw new ApiBusinessException((int)EnumCode.User, "Id account user required", System.Net.HttpStatusCode.NotFound, "Http");
+                if (String.IsNullOrEmpty(comands.Firstname))
+                    throw new ApiBusinessException((int)EnumCode.User, "Firstname required", System.Net.HttpStatusCode.NotFound, "Http");
+                if (String.IsNullOrEmpty(comands.Lastname))
+                    throw new ApiBusinessException((int)EnumCode.User, "Lastname required", System.Net.HttpStatusCode.NotFound, "Http");
+                if (comands.Location == null)
+                    throw new ApiBusinessException((int)EnumCode.User, "Location user required", System.Net.HttpStatusCode.NotFound, "Http");
+                if (comands.Location.CountryId == 0 || comands.Location.CountryId == null)
+                    throw new ApiBusinessException((int)EnumCode.User, "Country required", System.Net.HttpStatusCode.NotFound, "Http");
+                if (comands.Location.ProvinceId == 0 || comands.Location.ProvinceId == null)
+                    throw new ApiBusinessException((int)EnumCode.User, "Province required", System.Net.HttpStatusCode.NotFound, "Http");
+                if (comands.Location.CityId == 0 || comands.Location.CityId == null)
+                    throw new ApiBusinessException((int)EnumCode.User, "City required", System.Net.HttpStatusCode.NotFound, "Http");
+              
+
+                await _context.AddAsync( new Location {
+                    CityId = comands.Location.CityId,
+                    ProvinceId = comands.Location.ProvinceId,
+                    CountryId = comands.Location.CountryId,
+                    CreatedAt = DateTime.Now,
+                    state = (Int32)StateEnum.Activeted,
+                    User = new List<User> {
+                         new User
+                        {
+                            AccountId = comands.AccountId,
+                            LocationId = comands.LocationId,
+                            Firstname = comands.Firstname,
+                            Lastname = comands.Lastname,
+                            EmailAddress = comands.EmailAddress,
+                            Gender = comands.Gender,
+                            Address = comands.Address,
+                            NumberAddress = comands.NumberAddress,
+                            DateOfBirth = comands.DateOfBirth,
+                            state = (Int32)StateEnum.Activeted,
+                            CreatedAt = DateTime.Now
+                        }
+                    }
+                });
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Makaya/src/services/Customer/Customer.Service.EventHandlers/EventHandler/UserHandler/UserUpdateEventHandler.cs
@@ -0,0 +1,72 @@
+﻿using Customer.Domain;
+using Customer.Service.EventHandlers.Commands.User;
+using Customers.Persistence.DataBase;
+using Makaya.Resolver.Enum;
+using Makaya.Resolver.Eum;
+using Makaya.Resolver.IExceptions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Customer.Service.EventHandlers.EventHandler.UserHandler
+{
+    public class UserUpdateEventHandler :
+        INotificationHandler<UserUpdateCommand>
+    {
+        private readonly ApplicationDbContext _context;
+        public UserUpdateEventHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(UserUpdateCommand notification, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var entity =  _context.Users.Where(x => x.UserId == notification.UserId && x.state == (Int32)StateEnum.Activeted).Include("Location").ToList();
+                if (entity.Count > 0)
+                    throw new ApiBusinessException((int)EnumCode.Province, "User does not exist", System.Net.HttpStatusCode.NotFound, "Http");
+
+                if (notification.AccountId == 0 || notification.AccountId == null)
+                    throw new ApiBusinessException((int)EnumCode.User, "Id account user required", System.Net.HttpStatusCode.NotFound, "Http");
+                if (String.IsNullOrEmpty(notification.Firstname))
+                    throw new ApiBusinessException((int)EnumCode.User, "Firstname required", System.Net.HttpStatusCode.NotFound, "Http");
+                if (String.IsNullOrEmpty(notification.Lastname))
+                    throw new ApiBusinessException((int)EnumCode.User, "Lastname required", System.Net.HttpStatusCode.NotFound, "Http");
+                if (notification.Location == null)
+                    throw new ApiBusinessException((int)EnumCode.User, "Location user required", System.Net.HttpStatusCode.NotFound, "Http");
+                if (notification.Location.CountryId == 0 || notification.Location.CountryId == null)
+                    throw new ApiBusinessException((int)EnumCode.User, "Country required", System.Net.HttpStatusCode.NotFound, "Http");
+                if (notification.Location.ProvinceId == 0 || notification.Location.ProvinceId == null)
+                    throw new ApiBusinessException((int)EnumCode.User, "Province required", System.Net.HttpStatusCode.NotFound, "Http");
+                if (notification.Location.CityId == 0 || notification.Location.CityId == null)
+                    throw new ApiBusinessException((int)EnumCode.User, "City required", System.Net.HttpStatusCode.NotFound, "Http");
+
+                
+                entity.FirstOrDefault().Location.CityId = notification.Location.CityId;
+                entity.FirstOrDefault().Location.ProvinceId = notification.Location.ProvinceId;
+                entity.FirstOrDefault().Location.CountryId = notification.Location.CountryId;
+                entity.FirstOrDefault().Location.UpdatedAt = DateTime.Now;
+                entity.FirstOrDefault().Firstname = notification.Firstname;
+                entity.FirstOrDefault().Lastname = notification.Lastname;
+                entity.FirstOrDefault().EmailAddress = notification.EmailAddress;
+                entity.FirstOrDefault().Gender = notification.Gender;
+                entity.FirstOrDefault().Address = notification.Address;
+                entity.FirstOrDefault().NumberAddress = notification.NumberAddress;
+                entity.FirstOrDefault().DateOfBirth = notification.DateOfBirth;
+                entity.FirstOrDefault().UpdatedAt = DateTime.Now;
+
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
